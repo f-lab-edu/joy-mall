84818849b717eda13138921cecad030fa964b37d
hmmini <pak4184@naver.com>
2024-03-08 17:27:23 +0900

    first commit

---
+++ src/main/java/com/mini/joymall/JoyMallApplication.java
@@ -0,0 +1,13 @@
+package com.mini.joymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoyMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mini/joymall/JoyMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.joymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
