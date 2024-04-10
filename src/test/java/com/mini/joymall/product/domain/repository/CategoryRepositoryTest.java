33c9bd54ab900dc309671847ead16d0198b82dec
hmmini <pak4184@naver.com>
2024-04-10 21:03:53 +0900

    [test] 상품 검색 카테고리 API 테스트 코드 작성
    issue: 25

--- src/test/java/com/mini/joymall/product/domain/repository/CategoryRepositoryTest.java
+++ src/test/java/com/mini/joymall/product/domain/repository/CategoryRepositoryTest.java
@@ -1,36 +1,62 @@
 package com.mini.joymall.product.domain.repository;
 
 import com.mini.joymall.product.domain.entity.Category;
+import com.mini.joymall.product.dto.CategoryChildrenResponse;
+import com.mini.joymall.product.dto.CategoryDTO;
+import com.mini.joymall.product.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
+@Transactional
 class CategoryRepositoryTest {
     @Autowired
     CategoryRepository categoryRepository;
+    @Autowired
+    CategoryService categoryService;
 
-    @Test
-    void 카테고리_초기_데이터가_제대로_들어가는지_테스트() {
-        assertThat(categoryRepository.findAll().size()).isEqualTo(22);
+    @BeforeEach
+    void setup() {
+        categoryRepository.deleteAll();
+
+        Category category = categoryRepository.save(new Category(null, 0, "핸드폰/통신"));
+        Long categoryId = category.getId();
+
+        Category category1 = categoryRepository.save(new Category(categoryId, 1, "휴대폰기종"));
+        categoryRepository.saveAll(Arrays.asList(
+                new Category(categoryId, 1, "소재"),
+                new Category(categoryId, 1, "색상")
+        ));
+
+        Category category2 = categoryRepository.save(new Category(category1.getId(), 2, "아이폰 14"));
+        categoryRepository.saveAll(Arrays.asList(
+                new Category(category1.getId(), 2, "아이폰 13")
+        ));
+
+        categoryRepository.saveAll(Arrays.asList(
+                new Category(category2.getId(), 3, "256GB"),
+                new Category(category2.getId(), 3, "128GB")
+        ));
     }
 
     @Test
-    void 카테고리_0depth_조회시_초기데이터_개수와_같다() {
-        List<Category> all = categoryRepository.findByDepth(0);
-        assertThat(all.size()).isEqualTo(22);
+    void 카테고리_전체_조회() {
+        assertThat(categoryRepository.findAll().size()).isEqualTo(8);
     }
 
     @Test
-    void 카테고리_1depth_조회시_0개이다() {
+    void 카테고리_N_depth_조회() {
         List<Category> all = categoryRepository.findByDepth(1);
-        assertThat(all.size()).isEqualTo(0);
+        assertThat(all.size()).isEqualTo(3);
     }
 }
--- src/test/java/com/mini/joymall/product/service/CategoryServiceTest.java
+++ src/test/java/com/mini/joymall/product/service/CategoryServiceTest.java
@@ -2,6 +2,7 @@ package com.mini.joymall.product.service;
 
 import com.mini.joymall.product.domain.entity.Category;
 import com.mini.joymall.product.domain.repository.CategoryRepository;
+import com.mini.joymall.product.dto.CategoryChildrenResponse;
 import com.mini.joymall.product.dto.CategoryDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
@@ -13,7 +14,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
@@ -42,5 +45,39 @@ class CategoryServiceTest {
 
         // then
         assertThat(mockCategories.size()).isEqualTo(categories.size());
+    }
+
+    @Test
+    @DisplayName("자식 노드 조회 테스트")
+    void getCategoryChildren() {
+        Long categoryId = 1L;
+        Category category1 = new Category(categoryId, null, 0, "핸드폰/통신", null, null);
+        Category category2 = new Category(2L, categoryId, 0, "휴대폰기종", null, null);
+        Category category3 = new Category(3L, category2.getId(), 0, "아이폰14", null, null);
+        Category category4 = new Category(4L, category3.getId(), 0, "256GB", null, null);
+        Category category5 = new Category(5L, category3.getId(), 0, "128GB", null, null);
+
+        given(categoryRepository.findById(categoryId)).willReturn(Optional.of(category1));
+        given(categoryRepository.findByParentId(category1.getId())).willReturn(Collections.singletonList(category2));
+        given(categoryRepository.findByParentId(category2.getId())).willReturn(Collections.singletonList(category3));
+        given(categoryRepository.findByParentId(category3.getId())).willReturn(Arrays.asList(category4, category5));
+
+        List<CategoryChildrenResponse> response1 = categoryService.getCategoryChildren(1L);
+
+        assertThat(response1.size()).isEqualTo(1);
+        assertThat(response1.get(0).getName()).isEqualTo("핸드폰/통신");
+
+        List<CategoryChildrenResponse> response2 = response1.get(0).getChildren();
+        assertThat(response2.size()).isEqualTo(1);
+        assertThat(response2.get(0).getName()).isEqualTo("휴대폰기종");
+
+        List<CategoryChildrenResponse> response3 = response2.get(0).getChildren();
+        assertThat(response3.size()).isEqualTo(1);
+        assertThat(response3.get(0).getName()).isEqualTo("아이폰14");
+
+        List<CategoryChildrenResponse> response4 = response3.get(0).getChildren();
+        assertThat(response4.size()).isEqualTo(2);
+        assertThat(response4.get(0).getName()).isEqualTo("256GB");
+        assertThat(response4.get(1).getName()).isEqualTo("128GB");
     }
 }
