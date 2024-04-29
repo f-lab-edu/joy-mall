e1c72ef37aa14fa3e88f94b506ba20d503d549d5
hmmini <pak4184@naver.com>
2024-04-29 19:45:24 +0900

    [refactor] ResponseEntity.status(HttpStatus.OK) 공통화
    issue: 45

--- src/main/java/com/mini/joymall/commons/ApiResponse.java
+++ src/main/java/com/mini/joymall/commons/ApiResponse.java
@@ -1,12 +1,9 @@
 package com.mini.joymall.commons;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ApiResponse {
     public static <T> ResponseEntity<T> OK(T object) {
         return ResponseEntity.ok().body(object);
     }
-
-    public static final ResponseEntity.BodyBuilder OK = ResponseEntity.status(HttpStatus.OK);
 }
--- src/main/java/com/mini/joymall/customer/controller/AddressController.java
+++ src/main/java/com/mini/joymall/customer/controller/AddressController.java
@@ -1,12 +1,11 @@
 package com.mini.joymall.customer.controller;
 
-import com.mini.joymall.customer.domain.entity.Address;
+import com.mini.joymall.commons.ApiResponse;
 import com.mini.joymall.customer.dto.request.CreateAddressRequest;
 import com.mini.joymall.customer.dto.AddressDTO;
 import com.mini.joymall.customer.service.AddressService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +16,6 @@ public class AddressController {
 
     @PostMapping("/addresses")
     public ResponseEntity<AddressDTO> create(@RequestBody @Valid CreateAddressRequest addressDTO) {
-        return ResponseEntity.status(HttpStatus.OK).body(addressService.save(addressDTO));
+        return ApiResponse.OK(addressService.save(addressDTO));
     }
 }
