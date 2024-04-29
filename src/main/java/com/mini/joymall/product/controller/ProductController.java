6256bb486649f5a179ad4c9131fb9a7e4bea429e
hmmini <pak4184@naver.com>
2024-04-29 19:02:48 +0900

    [refactor] @RequestParam @ModelAttribute 를 이용하여 리팩토링
    - @ModelAttribute를 이용해서 DTO로 공통화 시켜줌
    
    issue: 44

--- src/main/java/com/mini/joymall/product/controller/ProductController.java
+++ src/main/java/com/mini/joymall/product/controller/ProductController.java
@@ -2,16 +2,14 @@ package com.mini.joymall.product.controller;
 
 import com.mini.joymall.product.dto.ProductDTO;
 import com.mini.joymall.product.dto.ProductPageResponse;
+import com.mini.joymall.product.dto.ProductSearchDTO;
 import com.mini.joymall.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,12 +25,9 @@ public class ProductController {
     }
 
     @GetMapping("/products/search")
-    public ResponseEntity<ProductPageResponse> search(
-            @RequestParam("name") String productName,
-            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
-            @RequestParam(value = "pageNumber", defaultValue = "0") int pageNumber) {
-        Pageable pageable = PageRequest.of(pageNumber, pageSize);
-        ProductPageResponse productPageResponse = productService.search(productName, pageable);
+    public ResponseEntity<ProductPageResponse> search(@ModelAttribute ProductSearchDTO productSearchDTO) {
+        Pageable pageable = PageRequest.of(productSearchDTO.getPageNumber(), productSearchDTO.getPageSize());
+        ProductPageResponse productPageResponse = productService.search(productSearchDTO.getKeyword(), pageable);
         return ResponseEntity.status(HttpStatus.OK).body(productPageResponse);
     }
 
