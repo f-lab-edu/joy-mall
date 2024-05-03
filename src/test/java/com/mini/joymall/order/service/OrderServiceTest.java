079bec1025b25431e1fe02413d1bd226bf61678b
hmmini <pak4184@naver.com>
2024-05-03 19:09:52 +0900

    [test] 주문 로직 테스트 코드 작성
    issue: 67

---
+++ src/test/java/com/mini/joymall/order/service/OrderServiceTest.java
@@ -0,0 +1,98 @@
+package com.mini.joymall.order.service;
+
+import com.mini.joymall.customer.domain.entity.Customer;
+import com.mini.joymall.customer.domain.entity.CustomerAddress;
+import com.mini.joymall.customer.domain.entity.Location;
+import com.mini.joymall.customer.domain.repository.CustomerAddressRepository;
+import com.mini.joymall.customer.domain.repository.CustomerRepository;
+import com.mini.joymall.order.domain.entity.OrderItem;
+import com.mini.joymall.order.domain.entity.OrderStatus;
+import com.mini.joymall.order.domain.repository.OrderItemRepository;
+import com.mini.joymall.order.dto.request.CreateOrderItemRequest;
+import com.mini.joymall.order.dto.request.CreateOrderRequest;
+import com.mini.joymall.order.dto.response.CreateOrderResponse;
+import com.mini.joymall.product.domain.entity.ProductOption;
+import com.mini.joymall.product.domain.repository.ProductOptionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OrderServiceTest {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerAddressRepository customerAddressRepository;
+
+    @Autowired
+    private ProductOptionRepository productOptionRepository;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Test
+    void 여러개의_주문_아이템을_담고_주문_성공() {
+        // given
+        Customer customer = new Customer("test@test.com", "1234", "test", "010-1234-4321");
+        Long customerId = customerRepository.save(customer)
+                .getId();
+
+        Location location = new Location("대한민국", "서울", "강남구", "도산대로", "010-123", "상세주소");
+        CustomerAddress customerAddress = new CustomerAddress(customerId, "test", "010-1234-4321", location);
+        customerAddressRepository.save(customerAddress);
+
+        ProductOption productOption1 = new ProductOption(1L, "딸기맛", 1000, 5);
+        ProductOption productOption2 = new ProductOption(1L, "초코맛", 1000, 3);
+        ProductOption savedProductOption1 = productOptionRepository.save(productOption1);
+        ProductOption savedProductOption2 = productOptionRepository.save(productOption2);
+
+        CreateOrderItemRequest createOrderItemRequest1 = new CreateOrderItemRequest(savedProductOption1.getId(), 5, 1000);
+        CreateOrderItemRequest createOrderItemRequest2 = new CreateOrderItemRequest(savedProductOption2.getId(), 3, 1000);
+        List<CreateOrderItemRequest> createOrderItemRequests = Arrays.asList(createOrderItemRequest1, createOrderItemRequest2);
+        CreateOrderRequest createOrderRequest = new CreateOrderRequest(customerId, createOrderItemRequests);
+
+        // when
+        CreateOrderResponse savedOrder = orderService.createOrder(createOrderRequest);
+        List<OrderItem> orderItems = savedOrder.getOrderItems().stream().toList();
+
+        // then
+        assertThat(savedOrder.getStatus()).isEqualTo(OrderStatus.PENDING);
+        assertThat(orderItems.size()).isEqualTo(2);
+        assertThat(orderItemRepository.findById(orderItems.get(0).getId()).isPresent()).isTrue();
+        assertThat(orderItemRepository.findById(orderItems.get(1).getId()).isPresent()).isTrue();
+        assertThat(productOptionRepository.findById(orderItems.get(0).getProductOptionId()).isPresent()).isTrue();
+        assertThat(productOptionRepository.findById(orderItems.get(1).getProductOptionId()).isPresent()).isTrue();
+    }
+
+    @Test
+    void 주문_수량이_재고를_넘으면_주문_실패() {
+        // given
+        Customer customer = new Customer("test@test.com", "1234", "test", "010-1234-4321");
+        Long customerId = customerRepository.save(customer)
+                .getId();
+
+        Location location = new Location("대한민국", "서울", "강남구", "도산대로", "010-123", "상세주소");
+        CustomerAddress customerAddress = new CustomerAddress(customerId, "test", "010-1234-4321", location);
+        customerAddressRepository.save(customerAddress);
+
+        ProductOption productOption1 = new ProductOption(1L, "딸기맛", 1000, 5);
+        ProductOption savedProductOption1 = productOptionRepository.save(productOption1);
+
+        CreateOrderItemRequest createOrderItemRequest1 = new CreateOrderItemRequest(savedProductOption1.getId(), 10, 1000);
+        List<CreateOrderItemRequest> createOrderItemRequests = List.of(createOrderItemRequest1);
+        CreateOrderRequest createOrderRequest = new CreateOrderRequest(customerId, createOrderItemRequests);
+
+        // when, then
+        assertThrows(IllegalArgumentException.class, () -> orderService.createOrder(createOrderRequest));
+    }
+}
